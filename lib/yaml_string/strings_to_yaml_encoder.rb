5f5796053c990b74a3608d6660710b44e68e3f4a
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2012-03-27 16:16:31 -0400

    Adds tests for parsing strings dictionary lines

--- lib/yaml_string/strings_to_yaml_encoder.rb
+++ lib/yaml_string/strings_to_yaml_encoder.rb
@@ -1,2 +1,46 @@
+class InvalidKeyError < StandardError; end
+class InvalidValueError < StandardError; end
+class InvalidTerminatorError < StandardError; end
+
 class StringsToYamlEncoder
+  attr_accessor :strings_array
+  def initialize(strings_array = [])
+    @strings_array = strings_array
+  end
+
+  # Returns a hash when given
+  def parse_strings_array(array)
+    raise ArgumentError.new("Invalid strings format: missing '{\\n'") if array.first != "{\n"
+    raise ArgumentError.new("Invalid strings format: missing '}'") if array.last != "}"
+
+  end
+
+  # Returns true if the value is a valid string NSString
+  #
+  # See http://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/PropertyLists/OldStylePlists/OldStylePLists.html#//apple_ref/doc/uid/20001012-BBCBDBJE
+  # for more info
+  def valid_string?(value)
+    (value       =~ /\s*"[^"]*"\s*/ ||
+     value.strip =~ /\A\S+\Z/) &&
+     value.strip != ';'                # handles case where no text in the value
+  end
+
+  # Returns true if the last character of the string, ignoring spaces, is a
+  # semi-colon.
+  def valid_terminator?(value)
+    return false if value.nil? || value == ""
+
+    value.gsub(' ', '')[-1] == ";"
+  end
+
+  #
+  def parse_strings_key_value(line)
+    key_string, value = line.split('=')
+
+    raise InvalidValueError.new("Invalid strings format: Missing value for line after '=': '#{line}'")     unless valid_string?(value)
+    raise InvalidKeyError.new("Invalid strings format: Missing key for line before '=': '#{line}'")        unless valid_string?(key_string)
+    raise InvalidTerminatorError.new("Invalid strings format: Missing key for line before '=': '#{line}'") unless valid_terminator?(value)
+
+    # keys              = key_string[1..-2].split('.')
+  end
 end
--- spec/lib/strings_to_yaml_encoder_spec.rb
+++ spec/lib/strings_to_yaml_encoder_spec.rb
@@ -4,4 +4,78 @@ describe StringsToYamlEncoder do
   it "should exist" do
     lambda { subject.should be_a(StringsToYamlEncoder) }.should_not raise_error(Exception)
   end
+
+  describe "parse_strings_array" do
+    it "should raise an error if missing '{'" do
+      lambda { subject.parse_strings_array(["}"]) }.should raise_error(ArgumentError)
+    end
+
+    it "should raise an error if missing '}'" do
+      lambda { subject.parse_strings_array(["{\n"]) }.should raise_error(ArgumentError)
+    end
+  end
+
+  describe "valid_string?" do
+    it "returns true for a quoted string" do
+      subject.valid_string?("\" \"").should be_true
+    end
+
+    it "returns true for another quoted string" do
+      subject.valid_string?("\"Something good here\"").should be_true
+    end
+
+    it "returns true for non-whitespace string" do
+      subject.valid_string?("something_good_here").should be_true
+    end
+
+    it "returns true for non-whitespace string with a terminator" do
+      subject.valid_string?("something_good_here;").should be_true
+    end
+
+    it "returns false for non-quoted space" do
+      subject.valid_string?(" ").should be_false
+    end
+
+    it "returns false for non-quoted string" do
+      subject.valid_string?("something good here").should be_false
+    end
+  end
+
+  describe "valid_terminator?" do
+    it "returns true if last character is a ;" do
+      subject.valid_terminator?(";").should be_true
+    end
+
+    it "returns true if last character is a ; ignoring spaces" do
+      subject.valid_terminator?("  ;   ").should be_true
+    end
+
+    it "returns false if last character is not a ; ignoring spaces" do
+      subject.valid_terminator?("  ").should be_false
+    end
+
+    it "returns false if last character is not a ;" do
+      subject.valid_terminator?(";a").should be_false
+    end
+  end
+
+  describe "parse_strings_key_value" do
+    it "should raise an error if there is no value for a line" do
+      lambda { subject.parse_strings_key_value("\"a.b\" = ;") }.should raise_error(InvalidValueError)
+    end
+
+    it "should raise an error if there is value for a line" do
+      lambda { subject.parse_strings_key_value("\"a.b\" = asdf asdf;") }.should raise_error(InvalidValueError)
+    end
+
+    it "should raise an error if there is no key for a line" do
+      lambda { subject.parse_strings_key_value(" = c;") }.should raise_error(InvalidKeyError)
+    end
+
+    it "should raise an error if there is no terminator" do
+      lambda { subject.parse_strings_key_value("a.b = c") }.should raise_error(InvalidTerminatorError)
+    end
+
+
+  end
 end
