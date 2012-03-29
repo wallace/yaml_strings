b0607bf2a8138c9039e018fadb1a8392e00b1090
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2012-03-28 21:22:53 -0400

    Delineates between class and instance methods for StringsToYamlEncoder

--- lib/yaml_strings/strings_to_yaml_encoder.rb
+++ lib/yaml_strings/strings_to_yaml_encoder.rb
@@ -9,74 +9,76 @@ class StringsToYamlEncoder
   end
 
   def to_s
-    parse_strings_array(@strings_array)
+    StringsToYamlEncoder.parse_strings_array(@strings_array)
   end
 
-  # Returns a hash when given
-  def parse_strings_array(array)
-    raise ArgumentError.new("Invalid strings format: missing '{\\n'") unless /{/.match(array.shift)
-    raise ArgumentError.new("Invalid strings format: missing '}'")    unless /}/.match(array.pop)
+  class << self
+    # Returns a hash when given
+    def parse_strings_array(array)
+      raise ArgumentError.new("Invalid strings format: missing '{\\n'") unless /{/.match(array.shift)
+      raise ArgumentError.new("Invalid strings format: missing '}'")    unless /}/.match(array.pop)
 
-    array.inject({}) do |hash,line|
-      key_string, value = parse_strings_key_value(line)
+      array.inject({}) do |hash,line|
+        key_string, value = parse_strings_key_value(line)
 
-      key_string = remove_quotes_from_key_string(key_string)
-      keys = key_string.split('.')
+        key_string = remove_quotes_from_key_string(key_string)
+        keys = key_string.split('.')
 
-      hash.merge!(nested_hash(keys + [value]))
+        hash.merge!(nested_hash(keys + [value]))
+      end
     end
-  end
 
-  # %{ a b c d } =>
-  #
-  # { a => { b => { c => d } } }
-  #
-  def nested_hash(keys)
-    first = keys.shift
-    return first if keys.empty? # base case
+    # %{ a b c d } =>
+    #
+    # { a => { b => { c => d } } }
+    #
+    def nested_hash(keys)
+      first = keys.shift
+      return first if keys.empty? # base case
 
-    hash        = Hash.new
-    hash[first] = nested_hash(keys)
-    hash
-  end
+      hash        = Hash.new
+      hash[first] = nested_hash(keys)
+      hash
+    end
 
-  # Returns true if the value is a valid string NSString
-  #
-  # See http://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/PropertyLists/OldStylePlists/OldStylePLists.html#//apple_ref/doc/uid/20001012-BBCBDBJE
-  # for more info
-  def valid_string?(value)
-    (value       =~ /\s*"[^"]*"\s*/ ||
-     value.strip =~ /\A\S+\Z/) &&
-     value.strip != ';'                # handles case where no text in the value
-  end
+    # Returns true if the value is a valid string NSString
+    #
+    # See http://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/PropertyLists/OldStylePlists/OldStylePLists.html#//apple_ref/doc/uid/20001012-BBCBDBJE
+    # for more info
+    def valid_string?(value)
+      (value       =~ /\s*"[^"]*"\s*/ ||
+       value.strip =~ /\A\S+\Z/) &&
+       value.strip != ';'                # handles case where no text in the value
+    end
 
-  # Returns true if the last character of the string, ignoring spaces, is a
-  # semi-colon.
-  def valid_terminator?(value)
-    return false if value.nil? || value == ""
+    # Returns true if the last character of the string, ignoring spaces, is a
+    # semi-colon.
+    def valid_terminator?(value)
+      return false if value.nil? || value == ""
 
-    value.gsub(' ', '')[-1] == ";"
-  end
+      value.gsub(' ', '')[-1] == ";"
+    end
 
-  def remove_quotes_from_key_string(key_string)
-    key_string = key_string.strip[1..-1] if key_string.strip[0]  == '"'
-    key_string = key_string.strip[0..-2] if key_string.strip[-1] == '"'
-    key_string
-  end
+    def remove_quotes_from_key_string(key_string)
+      key_string = key_string.strip[1..-1] if key_string.strip[0]  == '"'
+      key_string = key_string.strip[0..-2] if key_string.strip[-1] == '"'
+      key_string
+    end
 
-  # Validates the format
-  def parse_strings_key_value(line)
-    key_string, value = line.split('=')
+    # Validates the format
+    def parse_strings_key_value(line)
+      key_string, value = line.split('=')
 
-    key_string.strip!
-    value.strip!
+      key_string.strip!
+      value.strip!
 
-    raise InvalidValueError.new("Invalid strings format: Missing value for line after '=': '#{line}'")     unless valid_string?(value)
-    raise InvalidKeyError.new("Invalid strings format: Missing key for line before '=': '#{line}'")        unless valid_string?(key_string)
-    raise InvalidTerminatorError.new("Invalid strings format: Missing terminator ';' for line: '#{line}'") unless valid_terminator?(value)
+      raise InvalidValueError.new("Invalid strings format: Missing value for line after '=': '#{line}'")     unless valid_string?(value)
+      raise InvalidKeyError.new("Invalid strings format: Missing key for line before '=': '#{line}'")        unless valid_string?(key_string)
+      raise InvalidTerminatorError.new("Invalid strings format: Missing terminator ';' for line: '#{line}'") unless valid_terminator?(value)
 
-    value = value[0..-2] # Removes the ; from the value
+      value = value[0..-2] # Removes the ; from the value
 
-    return key_string, value
+      return key_string, value
+    end
   end
 end
--- spec/lib/strings_to_yaml_encoder_spec.rb
+++ spec/lib/strings_to_yaml_encoder_spec.rb
@@ -7,117 +7,120 @@ describe StringsToYamlEncoder do
 
   describe "#to_s" do
     it "should work" do
-      subject.parse_strings_array(
-        ["{\n", "en.app_name = awesome;", "}"]
-      ).should eql({ "en" => { "app_name" => "awesome" } })
+      subject.strings_array = ["{\n", "en.app_name = awesome;", "}"]
+      subject.to_s.should eql({ "en" => { "app_name" => "awesome" } })
     end
   end
 
-  describe "parse_strings_array" do
-    it "should raise an error if missing '{'" do
-      lambda { subject.parse_strings_array(["}"]) }.should raise_error(ArgumentError)
+  describe "class methods" do
+    subject { StringsToYamlEncoder }
+
+    describe ".parse_strings_array" do
+      it "should raise an error if missing '{'" do
+        lambda { subject.parse_strings_array(["}"]) }.should raise_error(ArgumentError)
+      end
+
+      it "should raise an error if missing '}'" do
+        lambda { subject.parse_strings_array(["{\n"]) }.should raise_error(ArgumentError)
+      end
+
+      it "should work" do
+        subject.parse_strings_array(
+          ["{\n", "en.app_name = awesome;", "}"]
+        ).should eql({ "en" => { "app_name" => "awesome" } })
+      end
     end
 
-    it "should raise an error if missing '}'" do
-      lambda { subject.parse_strings_array(["{\n"]) }.should raise_error(ArgumentError)
+    describe ".valid_string?" do
+      it "returns true for a quoted string" do
+        subject.valid_string?("\" \"").should be_true
+      end
+
+      it "returns true for another quoted string" do
+        subject.valid_string?("\"Something good here\"").should be_true
+      end
+
+      it "returns true for non-whitespace string" do
+        subject.valid_string?("something_good_here").should be_true
+      end
+
+      it "returns true for non-whitespace string with a terminator" do
+        subject.valid_string?("something_good_here;").should be_true
+      end
+
+      it "returns false for non-quoted space" do
+        subject.valid_string?(" ").should be_false
+      end
+
+      it "returns false for non-quoted string" do
+        subject.valid_string?("something good here").should be_false
+      end
     end
 
-    it "should work" do
-      subject.parse_strings_array(
-        ["{\n", "en.app_name = awesome;", "}"]
-      ).should eql({ "en" => { "app_name" => "awesome" } })
-    end
-  end
+    describe ".valid_terminator?" do
+      it "returns true if last character is a ;" do
+        subject.valid_terminator?(";").should be_true
+      end
 
-  describe "valid_string?" do
-    it "returns true for a quoted string" do
-      subject.valid_string?("\" \"").should be_true
-    end
+      it "returns true if last character is a ; ignoring spaces" do
+        subject.valid_terminator?("  ;   ").should be_true
+      end
 
-    it "returns true for another quoted string" do
-      subject.valid_string?("\"Something good here\"").should be_true
-    end
+      it "returns false if last character is not a ; ignoring spaces" do
+        subject.valid_terminator?("  ").should be_false
+      end
 
-    it "returns true for non-whitespace string" do
-      subject.valid_string?("something_good_here").should be_true
+      it "returns false if last character is not a ;" do
+        subject.valid_terminator?(";a").should be_false
+      end
     end
 
-    it "returns true for non-whitespace string with a terminator" do
-      subject.valid_string?("something_good_here;").should be_true
+    describe ".parse_strings_key_value" do
+      it "should raise an error if there is no value for a line" do
+        lambda { subject.parse_strings_key_value("\"a.b\" = ;") }.should raise_error(InvalidValueError)
+      end
+
+      it "should raise an error if there is value for a line" do
+        lambda { subject.parse_strings_key_value("\"a.b\" = asdf asdf;") }.should raise_error(InvalidValueError)
+      end
+
+      it "should raise an error if there is no key for a line" do
+        lambda { subject.parse_strings_key_value(" = c;") }.should raise_error(InvalidKeyError)
+      end
+
+      it "should raise an error if there is no terminator" do
+        lambda { subject.parse_strings_key_value("a.b = c") }.should raise_error(InvalidTerminatorError)
+      end
+
+      it "should work fine" do
+        key_string, value = subject.parse_strings_key_value("a.b = c;")
+        key_string.should eql("a.b")
+        value.should eql("c")
+      end
     end
 
-    it "returns false for non-quoted space" do
-      subject.valid_string?(" ").should be_false
+    describe ".remove_quotes_from_key_string" do
+      it "should remove \" from key_string" do
+        subject.remove_quotes_from_key_string('"a.b"').should eql("a.b")
+      end
+
+      it "should do nothing to the string" do
+        subject.remove_quotes_from_key_string('a.b').should eql("a.b")
+      end
     end
 
-    it "returns false for non-quoted string" do
-      subject.valid_string?("something good here").should be_false
-    end
-  end
+    describe ".nested_hash" do
+      it "handles one element arrays" do
+        subject.nested_hash([:a]).should eql(:a)
+      end
 
-  describe "valid_terminator?" do
-    it "returns true if last character is a ;" do
-      subject.valid_terminator?(";").should be_true
-    end
+      it "handles two element arrays" do
+        subject.nested_hash([:a, :b]).should eql({a: :b})
+      end
 
-    it "returns true if last character is a ; ignoring spaces" do
-      subject.valid_terminator?("  ;   ").should be_true
-    end
-
-    it "returns false if last character is not a ; ignoring spaces" do
-      subject.valid_terminator?("  ").should be_false
-    end
-
-    it "returns false if last character is not a ;" do
-      subject.valid_terminator?(";a").should be_false
-    end
-  end
-
-  describe "parse_strings_key_value" do
-    it "should raise an error if there is no value for a line" do
-      lambda { subject.parse_strings_key_value("\"a.b\" = ;") }.should raise_error(InvalidValueError)
-    end
-
-    it "should raise an error if there is value for a line" do
-      lambda { subject.parse_strings_key_value("\"a.b\" = asdf asdf;") }.should raise_error(InvalidValueError)
-    end
-
-    it "should raise an error if there is no key for a line" do
-      lambda { subject.parse_strings_key_value(" = c;") }.should raise_error(InvalidKeyError)
-    end
-
-    it "should raise an error if there is no terminator" do
-      lambda { subject.parse_strings_key_value("a.b = c") }.should raise_error(InvalidTerminatorError)
-    end
-
-    it "should work fine" do
-      key_string, value = subject.parse_strings_key_value("a.b = c;")
-      key_string.should eql("a.b")
-      value.should eql("c")
-    end
-  end
-
-  describe "remove_quotes_from_key_string" do
-    it "should remove \" from key_string" do
-      subject.remove_quotes_from_key_string('"a.b"').should eql("a.b")
-    end
-
-    it "should do nothing to the string" do
-      subject.remove_quotes_from_key_string('a.b').should eql("a.b")
-    end
-  end
-
-  describe "#nested_hash" do
-    it "handles one element arrays" do
-      subject.nested_hash([:a]).should eql(:a)
-    end
-
-    it "handles two element arrays" do
-      subject.nested_hash([:a, :b]).should eql({a: :b})
-    end
-
-    it "handles three element arrays" do
-      subject.nested_hash([:a, :b, :c]).should eql({a: { b: :c }})
+      it "handles three element arrays" do
+        subject.nested_hash([:a, :b, :c]).should eql({a: { b: :c }})
+      end
     end
   end
 end
