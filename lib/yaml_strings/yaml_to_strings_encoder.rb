8f565b409f464eb79af2bef6efc5b371e0d277b0
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2012-03-28 21:17:00 -0400

    Delineates between class and instance methods

--- lib/yaml_strings/yaml_to_strings_encoder.rb
+++ lib/yaml_strings/yaml_to_strings_encoder.rb
@@ -4,33 +4,35 @@ class YamlToStringsEncoder
     @yaml_hash = yaml_hash
   end
 
-  # Returns an array of dict properties as strings
-  #
-  # hsh - a hash to convert into the string representation
-  # prev_keys - a string representing the previous keys of enclosing hashes
-  #
-  # Ex: { a: :b } # => "a" = "b";
-  # Ex: { a: { b: :c } } # => "a.b" = "c";
-  def convert_hash_to_dict_property(hsh, prev_keys = nil)
-    result = []
-    hsh.each_pair {|k,v|
-      if v.is_a?(Hash)
-        new_prev_keys  = "#{[prev_keys, k].compact.join('.')}"
-        result        += convert_hash_to_dict_property(v, new_prev_keys)
-      else
-        result        += ["\"#{[prev_keys, k].compact.join('.')}\" = \"#{v}\" ;"]
-      end
-    }
-    result
-  end
-
-  # Returns a string representing an old ASCII style property list
-  def to_property_dict_string(array_of_dict_propertes)
-    (["{"] + array_of_dict_propertes + ["}", nil]).join("\n")
-  end
-
   def to_s
-    array = convert_hash_to_dict_property(yaml_hash)
-    to_property_dict_string(array)
+    array = YamlToStringsEncoder.convert_hash_to_dict_property(yaml_hash)
+    YamlToStringsEncoder.to_property_dict_string(array)
+  end
+
+  class << self
+    # Returns an array of dict properties as strings
+    #
+    # hsh - a hash to convert into the string representation
+    # prev_keys - a string representing the previous keys of enclosing hashes
+    #
+    # Ex: { a: :b } # => "a" = "b";
+    # Ex: { a: { b: :c } } # => "a.b" = "c";
+    def convert_hash_to_dict_property(hsh, prev_keys = nil)
+      result = []
+      hsh.each_pair {|k,v|
+        if v.is_a?(Hash)
+          new_prev_keys  = "#{[prev_keys, k].compact.join('.')}"
+          result        += convert_hash_to_dict_property(v, new_prev_keys)
+        else
+          result        += ["\"#{[prev_keys, k].compact.join('.')}\" = \"#{v}\" ;"]
+        end
+      }
+      result
+    end
+
+    # Returns a string representing an old ASCII style property list
+    def to_property_dict_string(array_of_dict_propertes)
+      (["{"] + array_of_dict_propertes + ["}", nil]).join("\n")
+    end
   end
 end
--- spec/lib/yaml_to_strings_encoder_spec.rb
+++ spec/lib/yaml_to_strings_encoder_spec.rb
@@ -1,41 +1,45 @@
 require "spec_helper"
 
 describe YamlToStringsEncoder do
-  describe "convert_hash_to_dict_property" do
-    it "converts a simple hash" do
-      result = subject.convert_hash_to_dict_property({ a: :b })
-      result.first.should eql('"a" = "b" ;')
+  describe "class methods" do
+    subject { YamlToStringsEncoder }
+
+    describe ".convert_hash_to_dict_property" do
+      it "converts a simple hash" do
+        result = subject.convert_hash_to_dict_property({ a: :b })
+        result.first.should eql('"a" = "b" ;')
+      end
+
+      it "converts a nested hash" do
+        result = subject.convert_hash_to_dict_property({ a: { b: :c } })
+        result.first.should eql('"a.b" = "c" ;')
+      end
     end
 
-    it "converts a nested hash" do
-      result = subject.convert_hash_to_dict_property({ a: { b: :c } })
-      result.first.should eql('"a.b" = "c" ;')
-    end
-  end
-
-  describe "to_property_dict_string" do
-    it "should handle an empty array" do
-      subject.to_property_dict_string([]).should eql(
+    describe ".to_property_dict_string" do
+      it "should handle an empty array" do
+        subject.to_property_dict_string([]).should eql(
 <<STRINGS
 {
 }
 STRINGS
-)
-    end
+        )
+      end
 
-    it "should properly format a dict_property" do
-      dict_property_string = '"a.b" = "c"; '
-      subject.to_property_dict_string([dict_property_string]).should eql(
+      it "should properly format a dict_property" do
+        dict_property_string = '"a.b" = "c"; '
+        subject.to_property_dict_string([dict_property_string]).should eql(
 <<STRINGS
 {
 #{dict_property_string}
 }
 STRINGS
-)
+        )
+      end
     end
   end
 
-  describe "to_s" do
+  describe "#to_s" do
     it "should create a properly formatted 'strings' string" do
       subject.yaml_hash = { a: { b: :c } }
       subject.to_s.should eql(
@@ -44,7 +48,7 @@ STRINGS
 "a.b" = "c" ;
 }
 STRINGS
-)
+      )
     end
   end
 end
